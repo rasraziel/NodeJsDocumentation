70b024343c956c330649d210f35219f6aca34c35
Pupkin <rasraziel@gmail.com>
2021-03-04 21:54:51 +0100

    homepage done

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
+//Imports
 const express = require("express");
+
+
 const app = express();
-app.use(express.json());
+const port = 8080;
 
+app.use(express.static(__dirname + '/public'));
 
+//Navigation
+app.get('', (req, res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+});
 
-app.listen(8080, err =>{
+app.get('/getting-started', (req, res)=>{
+    res.sendFile(__dirname + "/public/getting-started.html");
+});
+
+app.get('/js', (req, res)=>{
+    res.sendFile(__dirname + "/public/js.html");
+});
+
+app.get('/node-modules', (req, res)=>{
+    res.sendFile(__dirname + "/public/node-modules.html");
+});
+
+app.get('/rest-api', (req, res)=>{
+    res.sendFile(__dirname + "/public/rest.html");
+});
+
+app.get('/express', (req, res)=>{
+    res.sendFile(__dirname + "/public/express.html");
+});
+
+app.listen(port, err =>{
     if (err)  
       console.log(err);
     else
-      console.log("Server running on port", 8080);  
+      console.log("Server running on port", port);  
   });
